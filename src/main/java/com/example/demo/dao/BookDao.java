797b77586a669196cd47edc6bc537c2b7d205272
qi.yixi <qi.yixi@moneyforward.co.jp>
2022-11-16 19:17:43 +0900

    Add Users and Book's JDBC.

--- src/main/java/com/example/demo/dao/BookDao.java
+++ src/main/java/com/example/demo/dao/BookDao.java
@@ -1,13 +1,22 @@
 package com.example.demo.dao;
 
 import com.example.demo.pojo.Book;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
 
 
 import java.util.List;
 
 public interface BookDao {
+    @Select("select * from libsystem.Book")
     List<Book> getBookList();
-    Book findBook(String title);
+    @Select("select * from libsystem.Book where Title = #{title}")
+    Book findBookbyTitle(String title);
+    @Select("select * from libsystem.Book where Author = #{Author}")
+    Book findBookbyAuthor(String Author);
+    @Insert("insert INTO libsystem.Book(Title,Author,ISBN) values(#{Title},#{Author},#{ISBN})")
     boolean addBook(Book newbook);
-    boolean deletBook(Book newbook);
+    @Delete("delete from libsystem.Book where BookID = #{BookID}")
+    boolean deletBook(int BookID);
 }
--- src/main/java/com/example/demo/dao/UsersDao.java
+++ src/main/java/com/example/demo/dao/UsersDao.java
@@ -1,14 +1,23 @@
 package com.example.demo.dao;
 
 import com.example.demo.pojo.Users;
+import lombok.Data;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
 public interface UsersDao {
     List<Users> getUserList();
+    @Select("select * from libsystem.Useres where Nickname = #{Nickname} ")
     Users checkUser(String NickName);
     boolean userRegister(Users users);
-    boolean updateUser(Users users);
+    @Select("select * from libsystem.Users where UserID = #{UserID}")
+    Users getUserbyId(int UserID);
+    @Update("update libsystem.Users set Nickname = #{Nickname},Password = #{Password},Email = #{Email} where UserID = #{UserID}")
+    int updateUser(Users users);
+    @Delete("delete from libsystem.Users where UserID = #{UserID}")
     boolean deleteUser(Users users);
 
 
